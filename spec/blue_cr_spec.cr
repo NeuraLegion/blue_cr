bc636fd61a329089e0d2e7e4d8b60d31d81ea647
bararchy <bar.hofesh@safe-t.com>
2017-11-28 23:09:09 +0200

    Moving to work with dbus

--- spec/blue_cr_spec.cr
+++ spec/blue_cr_spec.cr
@@ -1,5 +1,18 @@
-require "./spec_helper"
+require "dbus"
 
-describe BlueCr do
-  # TODO: Write tests
-end
+bus = DBus::Bus.new(DBus::BusType::SYSTEM)
+
+dest = bus.destination("org.bluez")
+puts "Dest: #{dest}"
+inter = dest.interface("org.freedesktop.DBus.Introspectable")
+p inter.call("Introspect").reply
+obj = dest.object("/org/bluez/hci1")
+puts "Obj: #{obj}"
+int = obj.interface("org.bluez.Adapter1")
+prop = obj.interface("org.freedesktop.DBus.Properties")
+puts "Interface: #{int}"
+p int.call("StartDiscovery").reply
+p int.call("GetDiscoveryFilters").reply
+sleep 2
+p int.call("StopDiscovery").reply
+p prop.call("Get", ["org.bluez.Adapter1", "Address"]).reply
