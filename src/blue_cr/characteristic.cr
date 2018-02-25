f5755b33559ec866fe62f71fae1e791140ce75ca
Bar Hofesh <bar.hofesh@gmail.com>
2018-02-25 11:49:05 +0200

    Characteristics

---
+++ src/blue_cr/characteristic.cr
@@ -0,0 +1,31 @@
+module BlueCr
+  class Characteristic
+    getter :object, :interface, :all_properties
+    @all_properties : Hash(DBus::Type, DBus::Type)
+
+    def initialize(@object : DBus::Object, @interface : DBus::Interface, @proporties : DBus::Interface)
+      @all_properties = get_all
+    end
+
+    def uuid
+      name = @all_properties["UUID"]?
+      if name.is_a?(DBus::Variant)
+        name.value
+      else
+        name
+      end
+    end
+
+    def characteristic_type
+      BlueCr::DbGattCharacteristics.by_uuid(uuid.to_s)
+    end
+
+    def refresh
+      @all_properties = get_all
+    end
+
+    private def get_all
+      @proporties.call("GetAll", ["org.bluez.GattCharacteristic1"]).reply.first.as(Hash(DBus::Type, DBus::Type))
+    end
+  end
+end
